6df1cb234662f857d1d762d8e42da2bde7ddb64a
ALABI GBENGA RAPHAEL <Raph4sure007@gmail.com>
2026-02-19 23:16:24 +0100

    feat: update About Me section description and social media icons.

--- src/components_main/sections/About.jsx
+++ src/components_main/sections/About.jsx
@@ -3,52 +3,65 @@ import ComputerModelContainer from "./services/computer/ComputerModelContainer";
 import { Link } from "react-router-dom";
 
 function About() {
-  return (
-      <div className="grid grid-rows-[auto_1fr_auto] h-screen">
-          <div>
-              <h1 className="text-2xl font-bold text-center">About Me</h1>
-              {/* <p className="text-[1rem] text-justify w-[95%] mx-auto -mb-30">
+    return (
+        <div className="grid grid-rows-[auto_1fr_auto] h-screen">
+            <div>
+                <h1 className="text-2xl font-bold text-center">About Me</h1>
+                {/* <p className="text-[1rem] text-justify w-[95%] mx-auto -mb-30">
           Am a highly motivated and focused technology professional with
           expertise in developing scalable solutions, analyzing complex data,
           and delivering innovative software applications across multiple
           platforms and technologies.
         </p> */}
-              <p className="text-[1rem] text-justify w-[95%] mx-auto -pb-20">
-                  I am a Data-Driven Full-Stack Engineer with 3+ years of
-                  experience and a B.Sc. in Statistics. I specialize in building
-                  secured and responsive web applications, ranging from an
-                  offline quiz app that cut grading time by 90% to the 'Digital
-                  Doctor' telemedicine platform that connect patients with
-                  healthcare providers online. Leveraging my background in
-                  Backend Engineering (ALX), Software Development (PLP) and Data
-                  Science (3MTT). I am currently mastering Cloud Infrastructure
-                  and DevOps to build resilient, automated solutions.
-              </p>
-          </div>
-          <div className="place-self-center w-full h-full">
-              <ComputerModelContainer />
-          </div>
-          <div className="w-full bg-blue-500 h-full grid grid-cols-3 place-items-center">
-              <aside>
-                  <p>&copy; 2025 Alabi Raphael</p>
-              </aside>
-              <aside className="flex gap-3">
-                  <Link>
-                      <img src="/footer_img/github.png" alt="Github Logo" />
-                  </Link>
-                  <Link>
-                      <img src="/footer_img/linkedin.png" alt="LinkedIn Logo" />
-                  </Link>
-                  <Link>
-                      <img src="/footer_img/twitter.png" alt="Twitter Logo" />
-                  </Link>
-              </aside>
-              <aside>
-                  <p>All rights reserved</p>
-              </aside>
-          </div>
-      </div>
-  );
+                <p className="text-[clamp(0.8rem,2vw,1rem)] text-justify w-[95%] mx-auto -pb-20">
+                    I am a Data-Driven Full-Stack Software Engineer with 3+
+                    years of experience and a B.Sc. in Statistics. I specialize
+                    in building secured and responsive web applications, ranging
+                    from an offline quiz app that cut grading time by 90% to the
+                    'Digital Doctor' telemedicine platform that connect patients
+                    with healthcare providers online. Leveraging my background
+                    in Backend Engineering (ALX), Software Development (PLP) and
+                    Data Science (3MTT). I am currently mastering Cloud
+                    Infrastructure and DevOps to build resilient, automated
+                    solutions.
+                </p>
+            </div>
+            <div className="place-self-center w-full h-full">
+                <ComputerModelContainer />
+            </div>
+            <div className="w-full bg-blue-500 h-full grid grid-cols-3 place-items-center text-[clamp(0.5rem,2vw,1rem)] my-1">
+                <aside>
+                    <p>&copy; 2025 raphweb.tech</p>
+                </aside>
+                <aside className="flex gap-1">
+                    <Link>
+                        <img
+                            src="/footer_img/github.svg"
+                            alt="Github Logo"
+                            className="w-[clamp(0.5rem,1.7vw,1.5rem)] object-contain"
+                        />
+                    </Link>
+                    <Link>
+                        <img
+                            src="/footer_img/linkedin.png"
+                            alt="LinkedIn Logo"
+                            className="w-[clamp(1rem,2vw,2rem)] object-contain"
+                        />
+                    </Link>
+                    <Link>
+                        <img
+                            src="/footer_img/twitter.png"
+                            alt="Twitter Logo"
+                            className="w-[clamp(1rem,2vw,2rem)] object-contain"
+                        />
+                    </Link>
+                </aside>
+                <aside>
+                    <p>All rights reserved</p>
+                </aside>
+            </div>
+        </div>
+    );
 }
 
 export default About;
